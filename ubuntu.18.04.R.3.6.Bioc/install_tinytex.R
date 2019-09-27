83cd8683c4cdafcbfabe36bbb10d6b03d640f179
jmonlong <jmonlong@ucsc.edu>
2019-09-27 10:36:50 -0700

    r docker images

--- ubuntu.18.04.R.3.6.Bioc/install_tinytex.R
+++ ubuntu.18.04.R.3.6.Bioc/install_tinytex.R
@@ -1,2 +1,6 @@
 install.packages('tinytex')
 tinytex::install_tinytex()
+
+tinytex::tlmgr_install(c('multirow', 'wrapfig', 'colortbl', 'tabu',
+                         'varwidth', 'threeparttable', 'threeparttablex',
+                         'environ', 'trimspaces', 'ulem', 'makecell', 'xcolor')
